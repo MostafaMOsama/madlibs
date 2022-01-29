364560b228474749979b28807ad6fb1e627f645e
Mostafa Mohamed Osama <Mostafa-m-osama@outlook.com>
2022-01-29 20:14:34 +0300

    Initial commit

---
+++ random_madlibs.py
@@ -0,0 +1,9 @@
+from sample_madlibs import hp, code, zombie, hungergames, madlibs
+import random
+
+if __name__ == "__main__":
+    m = random.choice([hp, code, zombie, hungergames, madlibs])
+    m.madlib()
+
+
+# First App with kying By Me 💓
